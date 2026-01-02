0fd1050360039a842517df5ff7b2c42049d15f54
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-01-02 17:15:28 +0100

    first commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -30,7 +30,7 @@ export function Navbar() {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300  ${
+      className={`fixed top-0 left-0 right-0 z-[9999] transition-all duration-300  ${
         isScrolled ? 'bg-black/90 backdrop-blur-lg' : 'bg-transparent'
       }`}
       // style={{ marginTop: '48px' }}
